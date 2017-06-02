d3c0519d4f19cd7ea1a68e028fd91f441988828d
Brian Brennan <me@brianbrennan.com>
2017-06-02 16:32:11 -0400

    init config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: {
+        javascript: './app/main/app.js'
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: [
+                    path.resolve(__dirname, '/node_modules/')
+                ],
+                loaders: ['react-hot-loader', 'babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader?sourceMap!sass-loader?sourceMap'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+
+    plugins: [HTMLWebpackPluginConfig]
+};
