bdae403733053e0d76858247bc2924cd05ce5be8
Brian Brennan <me@brianbrennan.com>
2017-06-05 12:22:10 -0400

    other nonesense as well

--- app/main/app.js
+++ app/main/app.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Menu from './../globals/components/menu/Menu';
+import Page from './../globals/components/pages/Page';
+import { Router, Route, browserHistory } from 'react-router';
+
+import './../globals/scss/style.scss';
 
 ReactDOM.render(
-    <h1>Hello Everyone</h1>,
+    <div className="app-wrapper">
+        <Menu></Menu>
+        <Router history={browserHistory}>
+            <Route path="/" component={Page} />
+        </Router>
+    </div>,
     document.getElementById('app')
 );
