9a979df71e1a2809a483420f1903cd66f0749458
Brian Brennan <me@brianbrennan.com>
2017-06-05 12:21:54 -0400

    Just some other nonesense

---
+++ app/globals/components/pages/Page.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Page extends Component {
+    render() {
+        return (
+            <div className='Page'>
+                <h1>Page 1</h1>
+            </div>
+        );
+    }
+}
